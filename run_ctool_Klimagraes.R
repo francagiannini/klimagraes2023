d32f662dfccf1971c0686fede92391d4bdb98926
Franca Giannini-Kurina <au710823@uni.au.dk>
2023-02-07 07:54:14 +0100

    comit 07/02 abandom basement

---
+++ run_ctool_Klimagraes.R
@@ -0,0 +1,85 @@
+# take aver write the file scenarios and then run the functions 
+
+run_in_folder <- function(i) {
+  #browser()
+  # Make a folder with scenario ID name
+  folder_name <-
+    paste(
+      "O:\\Tech_AGRO\\Jornaer\\Franca\\klimagraes2023/output/",
+      i[[1]],
+      sep = ""
+    )
+  
+  dir.create(folder_name)
+  # set the working 
+  
+  setwd(folder_name)
+  
+  # Write data txt
+  
+  write.table(as.data.frame(i[[2]]), 
+              paste(folder_name, "\\data.txt", sep = ""), 
+              sep ="\t",
+              dec = ".",
+              row.names = FALSE,
+              quote=FALSE)
+  
+  # Write input .txt
+  
+  writeLines(i[[3]], paste(folder_name, "\\input.txt", sep = ""), sep = "\t")
+  
+  # Make runscenarios .txt 
+  
+  runscn <-
+    c(paste("input", paste(folder_name, "\\input.txt", sep = ""), sep = "\t"),
+      paste("data", paste(folder_name, "\\data.txt", sep = ""), sep = "\t"),
+      paste("TemperatureData",
+            paste("O:\\Tech_AGRO\\Jornaer\\Franca\\klimagraes2023\\data/",
+                  i[[4]], sep =""), sep = "\t"),
+      paste("outputDir", paste(folder_name, "\\", sep = ""),  sep = "\t")
+    )
+  
+  writeLines(runscn, paste(folder_name,
+                           "\\runsenarios.txt",
+                           sep = ""))
+  
+  
+  writeLines("0", paste(folder_name, 
+                        "\\mode.txt", 
+                        sep = ""))
+  
+  # write.table(0, paste(folder_name, 
+  #                            "\\mode.txt", 
+  #                            sep = ""),  
+  #            sep = "\t",
+  #            row.names = FALSE,
+  #            quote=FALSE)
+  
+  # copy ctool app
+  
+  file.copy(
+    from = "O:\\Tech_AGRO\\Jornaer\\Franca\\klimagraes2023/data/ctool2.3.exe",
+    to = paste(folder_name,
+               "\\ctool2.3.exe",
+               sep = ""))
+  
+  # Execute .exe in each folder
+  
+  system2(paste(folder_name,
+                "\\ctool2.3.exe",
+                sep = ""))
+  
+  # delete executable 
+  
+  file.remove(from = paste(folder_name,
+                           "\\ctool2.3.exe",
+                           sep = ""))
+  
+  setwd("O:\\Tech_AGRO\\Jornaer\\Franca\\klimagraes2023")
+  
+}
+
+
+#aver_plot <- readRDS("aver_plot.RDS")
+
+lapply(aver_plot, run_in_folder)
